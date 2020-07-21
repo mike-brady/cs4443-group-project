d23f45d1efe0a2e6073fe36fb0336a6ca84ffbae
Mike Brady <brady2590@gmail.com>
2020-07-20 20:52:11 -0400

    Removed placeholder insert statements

--- database/setup/data.sql
+++ database/setup/data.sql
@@ -1,6 +1,1 @@
 USE restaurant;
-
-INSERT INTO Authors (FirstName,LastName) VALUES ('Harvey','Deitel');
-INSERT INTO Authors (FirstName,LastName) VALUES ('Paul','Deitel');
-INSERT INTO Authors (FirstName,LastName) VALUES ('Andrew','Goldberg');
-INSERT INTO Authors (FirstName,LastName) VALUES ('David','Choffnes');
