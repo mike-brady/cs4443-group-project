2290d7a847a967756e0a8129687f40e70130b40f
Mike Brady <brady2590@gmail.com>
2020-07-22 10:44:29 -0400

    Fixed menu item data

--- database/setup/data.sql
+++ database/setup/data.sql
@@ -3,5 +3,5 @@ USE restaurant;
 INSERT INTO items (name, description, price) VALUES
   ("Basic Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink", 7),
   ("Ultra Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink\nUltra Taters", 10),
-  ("Ultra Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink\nUltra Taters\nMEGA MILKSHAKE", 15)
+  ("MEGA Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink\nUltra Taters\nMEGA MILKSHAKE", 15)
 ;
