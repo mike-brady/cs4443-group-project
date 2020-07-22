65338bad1ba13580b9bc5fa016f567cb5ef28db1
Mike Brady <brady2590@gmail.com>
2020-07-21 20:02:53 -0400

    Added menu items to database

--- database/setup/data.sql
+++ database/setup/data.sql
@@ -1,1 +1,7 @@
 USE restaurant;
+
+INSERT INTO items (name, description, price) VALUES
+  ("Basic Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink", 7),
+  ("Ultra Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink\nUltra Taters", 10),
+  ("Ultra Meal", "Hamburger Palace Burger\nFrench-Fries\nDrink\nUltra Taters\nMEGA MILKSHAKE", 15)
+;
